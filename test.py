53d4c1d579612acc425d6ad3ef1bf6072dd592af
osc <73138388@continental.edu.pe>
2026-09-17 19:29:07 -0500

    Actualizando el codigo

--- test.py
+++ test.py
@@ -2,5 +2,6 @@
 nombre = input("Ingresa tu nombre: ")
 
 edad = int(input("Ingresa tu edad: "))
+apodo = input("Ingresa tu apodo: ")
 
-print(f"Hola {nombre}, tienes {edad} años.")
+print(f"Hola {nombre}, tu apodo es {apodo} y tienes {edad} años.")
